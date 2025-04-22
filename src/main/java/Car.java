a0b7bb8e7735cff1597ee077069ff8732cad41f2
Alex <belyaevalexander9@gmail.com>
2025-04-22 20:33:17 +0900

    Домашка была сложной. Понятно что работа с неизвестным материалом подразумевает поиск информации, но в рамках курса лучше было бы данную информацию всю предоставить. Либо изменить задачу. Я про scanner

---
+++ src/main/java/Car.java
@@ -0,0 +1,9 @@
+public class Car { // класс машина
+    String name = ""; // имени name присваиваем изначальное значение
+    int speed = 0; // имени speed присваиваем изначальное значение
+
+    public Car(String name, int speed) { // конструктор с параметрами name и speed.
+        this.name = name; // имя машины
+        this.speed = speed; // скорость машины
+    }
+}
---
+++ src/main/java/Race.java
@@ -0,0 +1,18 @@
+public class Race { // класс гонка.
+    String leader = ""; // имени leader присваиваем изначальное значение
+    int distance = 0; // имени distance присваиваем изначальное значение
+
+    public  int formulaDistance(Car car) { // метод формула дистанции с параметром класса Car  и имени car
+        int distance = 24 * car.speed; // формула дистанции.
+        return distance; // возвращаем полученные вычесления
+    }
+
+    public  String currentLeader(Car car) { // конструктор текущего лидера с параметрами
+        int newDistance = formulaDistance(car); // новой дистанции присваиваем формулу
+        if (this.distance < formulaDistance(car)) { // условие если текущая дистанция меньше формулы дистанции
+            this.leader = car.name; // то, текущего лидера присваиваем названию машины
+            this.distance = newDistance; // то текущую дистанцию присваиваем к новой дистанции
+        }
+        return leader; // возвращаем лидера
+    }
+}
